dc35e0ebc7d0e0597a376523c7cda02dad088fa4
Nagaraju7084 <nagaraju.kailasa123@gmail.com>
2023-11-06 22:59:22 +0530

    config server implementation 061123

--- src/main/java/com/hotel/config/config/HotelConfigServer061123Application.java
+++ src/main/java/com/hotel/config/config/HotelConfigServer061123Application.java
@@ -2,8 +2,12 @@ package com.hotel.config.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class HotelConfigServer061123Application {
 
 	public static void main(String[] args) {
