4ead803e72cf8afb795651f32f30e4187213622b
Nagaraju7084 <nagaraju.kailasa123@gmail.com>
2023-11-06 20:58:33 +0530

    config server 061123

---
+++ src/main/java/com/hotel/config/config/HotelConfigServer061123Application.java
@@ -0,0 +1,13 @@
+package com.hotel.config.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelConfigServer061123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelConfigServer061123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/config/config/HotelConfigServer061123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.config.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelConfigServer061123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
